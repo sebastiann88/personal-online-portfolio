1c787d1f9f58312e0589717135c34cd620be5940
sebastiann88 <sebastian_chmura@hotmail.com>
2015-11-01 11:41:04 -0500

    feat(all): add initial lightbox set up

---
+++ js/lightbox.js
@@ -0,0 +1,46 @@
+// TYPOGRAPHY
+    
+    //BRIDGEHEAD
+    
+    $('#open-bridgehead-popup').magnificPopup({
+    items: [
+          {
+            src: 'http://placehold.it/1700x1100',
+            title: 'Bridgehead 1'
+          },
+                {
+            src: 'http://placehold.it/1700x1100',
+            title: 'Bridgehead 2'
+          },
+                {
+            src: 'http://placehold.it/1700x1100',
+            title: 'Bridgehead 3'
+          },
+                {
+            src: 'http://placehold.it/1700x1100',
+            title: 'Bridgehead 4'
+          },
+                {
+            src: 'http://placehold.it/1700x1100',
+            title: 'Bridgehead 5'
+          },
+        ],
+        gallery: {
+          enabled: true
+        },
+        type: 'image' // this is a default type
+    });
+
+
+
+
+
+
+
+
+
+// ILLUSTRATION
+
+// INTERACTIVE
+
+// MOTION
