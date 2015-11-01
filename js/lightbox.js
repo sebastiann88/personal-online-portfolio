a95411c83ecea54113a0d7c67e3059f09654f6ba
sebastiann88 <sebastian_chmura@hotmail.com>
2015-11-01 17:41:27 -0500

    feat(all): create a new vertical layout for an illustration project

--- js/lightbox.js
+++ js/lightbox.js
@@ -31,15 +31,39 @@
         type: 'image' // this is a default type
     });
 
-
-
-
-
-
-
-
-
 // ILLUSTRATION
+    
+    //TENNIS SERVE
+    
+    $('#open-tennis-serve-popup').magnificPopup({
+    items: [
+          {
+            src: 'http://placehold.it/1100x1700',
+            title: 'Tennis Serve 1'
+          },
+                {
+            src: 'http://placehold.it/1100x1700',
+            title: 'Tennis Serve 2'
+          },
+                {
+            src: 'http://placehold.it/1100x1700',
+            title: 'Tennis Serve 3'
+          },
+                {
+            src: 'http://placehold.it/1100x1700',
+            title: 'Tennis Serve 4'
+          },
+                {
+            src: 'http://placehold.it/1100x1700',
+            title: 'Tennis Serve 5'
+          },
+        ],
+        gallery: {
+          enabled: true
+        },
+        type: 'image' // this is a default type
+    }); 
+        
 
 // INTERACTIVE
 
