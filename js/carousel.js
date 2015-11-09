18787b7fe1c6438c1b87b60fa573be99cc8de668
sebastiann88 <sebastian_chmura@hotmail.com>
2015-11-09 18:11:09 -0500

    feat(all): create a carousel slideshow on home page

---
+++ js/carousel.js
@@ -0,0 +1,13 @@
+$(window).load(function() { 
+    $('.flexslider').flexslider({
+            animation: "slide",
+            controlsContainer: ".flex-container",
+            slideshow: true,
+            slideshowSpeed: 5000,
+            animationDuration: 600,
+            directionNav: true,
+            controlNav: true,
+            animationLoop: true,
+            pauseOnAction: true,
+    });
+});
