6907c2510e5c27a665a44775a0a66db5ad539657
sebastiann88 <sebastian_chmura@hotmail.com>
2015-11-09 20:01:23 -0500

    style(all): create button navigation on carousel

--- js/carousel.js
+++ js/carousel.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
     //CAROUSELL
     
     $('.home-page-carousel').slick({
+        fade: true,
         autoplay: true,
         autoplaySpeed: 3000,
-        pauseOnHover: false,
+        arrows: false,
+        dots: true,
         swipetoSlide: true,
     });
 });
