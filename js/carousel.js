7816ab08c9848666ffc1d6a9f514930e4ce2523a
sebastiann88 <sebastian_chmura@hotmail.com>
2015-11-09 19:32:13 -0500

    feat(all): create carousel sliding animation on home page

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    //CAROUSELL
+    
+    $('.home-page-carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+        swipetoSlide: true,
+    });
+});
